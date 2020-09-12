2563747d945b03612c4ec921eca00cd705d53415
Kay <joe_kayu@yahoo.com>
2020-09-12 11:27:57 +0100

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,8 @@ import styled from "styled-components";
 class Navbar extends Component {
   render() {
     return (
-      <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
-      <Link to="/">
-        <img src={'logo512.png'} alt="store" width="40" height="50" className="navbar-brand" />
-      </Link>
+      <NavWrapper className="navbar navbar-expand-sm bg-default navbar-dark px-sm-5">
+      <Link className="navbar-brand" to="/">Mini-<span class="bran">Netflix</span></Link>
       <button
         className="navbar-toggler"
         type="button"
@@ -30,38 +28,48 @@ class Navbar extends Component {
               Home <span className="sr-only">(current)</span>
             </Link>
           </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Favorites
+            </Link>
+          </li>
           <li class="nav-item dropdown">
             <Link to="/" className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              categories
+              Categories
             </Link>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className="dropdown-item" href="/">Popular</a>
+              <a className="dropdown-item" href="/">Top Rated</a>
+              <a className="dropdown-item" href="/">Now Playing</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="/">Settings</a>
             </div>
           </li>
         </ul>
-        <form className="form-inline d-none d-lg-block ml-auto my-2 my-lg-0">
+        <form className="form-inline ml-auto my-lg-0">
           <input
             className="form-control "
             type="search"
-            placeholder="Search"
+            placeholder="&#128269; Search Movies"
             aria-label="Search"
           />
-          <span className="btn btn-warning text-white my-2 my-sm-0" type="submit">
-            <i className="fas fa-search"></i>
-          </span>
+            <button type="button" className="btn btn-default">Submit</button>
         </form>
         <ul className="navbar-nav align-items-center ml-auto">
         <li className="nav-item ml-4">
             <Link to="/" className="nav-link">
-              contact us
+              Contact us
             </Link>
           </li>
+
+          {/* check if user is true */}
+          {/* <li > 
+            <img className="avi" src=""/>
+            <span className="hidden-xs">displayName</span>
+            <span className="logout"><button class="btn btn-default">Sign out</button></span>
+          </li> */}
           <li className="nav-item ml-4">
-            <Link to="/" className="nav-link">
-              login/signup
-            </Link>
+            <button className="btn">Sign in</button>
           </li>
         </ul>
         {/* <ProductConsumer>
@@ -84,11 +92,28 @@ class Navbar extends Component {
 }
 
 const NavWrapper = styled.nav`
- background: var(--mainPurple) !important;
- .nav-link {
-    color: var(--mainWhite);
-    font-size: 1.3rem;
-    text-transform: capitalize;
-  }
+//  background: var(--mainPurple) !important;
+//  .nav-link {
+//     color: var(--mainWhite);
+//     font-size: 1.3rem;
+//     text-transform: capitalize;
+//   }
+border-bottom: 2px solid rgba(255,255,255,0.65);
+.navbar-brand {
+  color:rgba(255,255,255,0.65);
+  font-size: 1.5rem;
+  font-weight: 400;
+  letter-spacing: 0.15rem;
+  text-transform: uppercase;
+}
+.bran {
+  color:var(--mainRed);
+  text-shadow:0 0 5px var(--mainRed);
+  font-weight: 600;
+}
+.btn{
+  background-color: var(--mainRed);
+  color: var(--mainWhite);
+}
 `;
 export default Navbar;
