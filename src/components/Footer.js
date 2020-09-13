446f85369cd46003d1cdcfecc59444d2e734de83
Kay <joe_kayu@yahoo.com>
2020-09-13 21:44:20 +0100

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-        <div className="container footer">
-          <p className="grey mt-4 white">
+        <div className="container footer ml-5">
+          <p className="mt-4 white">
             &copy; 2020{" "}
             <a className="" href="https://lollykrown.xyz">
               Ol&#250;wak&#225;y&#242;d&#233;.
