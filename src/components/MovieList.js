f5b906845dd9835f3a5ed4f8b023b8bcaf90567f
Kay <joe_kayu@yahoo.com>
2020-09-30 16:23:53 +0100

    .

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -35,7 +35,7 @@ export default function MovieList(props) {
       setPageTitle("Favorite Movies")
     let cachedFav = JSON.parse(localStorage.getItem("movies"));
     addMovies(cachedFav)
-    setLoading(true)
+    setLoading(false)
     }
 
     const getMovies = async (ti) => {
